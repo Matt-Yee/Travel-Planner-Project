cc578bdce9a5d4b0b8bef265f2f5ee87e80c4b11
danpatoneil <danpatoneil@gmail.com>
2024-03-18 20:46:33 -0400

    Added localstorage functionality

--- Assests/Script/script.js
+++ Assests/Script/script.js
@@ -4,26 +4,35 @@ $(function(){
 //start-date references the input field for the day the trip starts
 //end-date references the input field for the day the trip ends
 //tripLength refrences a span tag that is meant to change with the
-    var tripLength;
-    var dateFormat = "dd-mm-yy",
+    var tripLength = 0;
+    var tripStart = dayjs();
+    var tripEnd = dayjs();
+    var dateFormat = "MM/DD/YY",
     from = $( "#start-date" )
       .datepicker({
-        defaultDate: "+1w",
         changeMonth: true,
-        numberOfMonths: 1
+        numberOfMonths: 1,
+        defaultDate: this.date
       })
       .on( "change", function() {
         to.datepicker( "option", "minDate", getDate( this ) );
-        updateLength();
+        tripStart = dayjs($("#start-date").val());
+        localStorage.setItem("tripStart", tripStart);
+        getDates();
+        renderDates();
+        if($('#end-date').val()!='')updateLength();
       }),
     to = $( "#end-date" ).datepicker({
-      defaultDate: "+1w",
       changeMonth: true,
       numberOfMonths: 1
     })
     .on( "change", function() {
       from.datepicker( "option", "maxDate", getDate( this ) );
-      updateLength();
+      tripEnd = dayjs($("#end-date").val());
+      localStorage.setItem("tripEnd", tripEnd);
+      getDates();
+      renderDates();
+      if($('#start-date').val()!='')updateLength();
     });
 
   function getDate( element ) {
@@ -36,19 +45,24 @@ $(function(){
 
     return date;
   }
+  getDates();
+  renderDates();
   //runs every time you update the fields, calculates the difference between the dates with dayjs
   function updateLength(){
-    const startDayJS = dayjs($("#start-date").val());
-    const endDayJS = dayjs($("#end-date").val());
-    tripLength = endDayJS.diff(startDayJS, "day");
+    tripLength = tripEnd.diff(tripStart, "day")+1;
+    localStorage.setItem("tripLength", tripLength);
     $("#tripLength").text(tripLength);
 
   }
+  function renderDates(){
+      $("#tripLength").text(tripLength);
+      $("#start-date").val(dayjs(tripStart).format(dateFormat));
+      $("#end-date").val(dayjs(tripEnd).format(dateFormat));
+  }
 
-
-
-
-
-
-
+  function getDates(){
+      if(localStorage.getItem("tripLength")!=null)tripLength=localStorage.getItem("tripLength");
+      if(localStorage.getItem("tripStart")!=null)tripStart=dayjs(localStorage.getItem("tripStart"));
+      if(localStorage.getItem("tripEnd")!=null)tripEnd=dayjs(localStorage.getItem("tripEnd"));
+  }
 });
