7a3decb7d8426b3912606257b028d1ca36ff9fe5
seth <seth_philpot@yahoo.com>
2024-03-18 20:33:15 -0400

    added currency converter api, added page redirect javascript function

--- Assests/Script/script.js
+++ Assests/Script/script.js
@@ -1,5 +1,5 @@
 const budgetEl = document.getElementById("budget");
-const sendBtn = document.getElementById("send");
+const sendBtn = document.getElementById("submit");
 
 function getBudget(){
     const inputValue = budgetEl.value;
@@ -7,5 +7,26 @@ function getBudget(){
     localStorage.setItem("number", inputValue);
 }
 
+//added function in to link to activites page
+sendBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.replace("activites.html");
+    getBudget();
+})
 
-sendBtn.addEventListener("click", getBudget)
+
+//currency converter api
+const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://currency-converter241.p.rapidapi.com/conversion_rate?from=UYU&to=USD',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '99ba836070msh21a815cb6cbff68p1660a8jsn0470850fa436',
+		'X-RapidAPI-Host': 'currency-converter241.p.rapidapi.com'
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
