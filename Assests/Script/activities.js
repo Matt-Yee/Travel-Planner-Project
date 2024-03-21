f2cf4509019e629957a03cbe8356007c989dab2b
danpatoneil <danpatoneil@gmail.com>
2024-03-20 20:21:25 -0400

    moved budget tag into the nav bar

--- Assests/Script/activities.js
+++ Assests/Script/activities.js
@@ -3,8 +3,14 @@ var tripBudget = localStorage.getItem("budget");
 //selects element in html with id of x from activities page
 var budgetEl = document.getElementById("x")
 
-console.log(x);
-
 //accesses the content of the budgetEl, sets the innerHTML property of the budgetEL element to teh value stored in the tripBudget variable.
 budgetEl.innerHTML = tripBudget;
 
+
+$('#activityList').on('click', function(event){
+    if(event.target.nodeName==="BUTTON"){
+        console.log("button works, boss");
+    }else{
+        console.log('Gotta click the button, boss');
+    }
+});
