581e2bdedde24621486530674bf9da2d1b504ac2
danpatoneil <danpatoneil@gmail.com>
2024-03-21 20:29:03 -0400

    altered card generation, added day period names to buttons

--- Assests/Script/activities.js
+++ Assests/Script/activities.js
@@ -8,7 +8,6 @@ budgetEl.innerHTML = tripBudget;
 //this sees if anything on the activitylist was clicked, if it was a button then it logs the ID of the button clicked. I assume we will use this to inform the modal what info to load.
 $('#activityList').on('click', function(event){
     if(event.target.nodeName==="BUTTON"){
-        console.log(event.target.id);
     }
 });
 //draws page, run at the start of the page loading and should be run again every time the modal is exited to present new data
@@ -30,11 +29,11 @@ function generateDay(date){
     const afternoon = generateCard('afternoon', date);
     const evening = generateCard('evening', date);
     const dateParsed = dayjs(date, 'MM-DD-YY').format('dddd, MMMM DD');
-    const dateHeader = $(`<h3>${dateParsed}</h3>`);
+    const budget = `Budget: ${localStorage.getItem('budget')} ${localStorage.getItem('targetCurrency')}`;
+    const dateHeader = $(`<h3>${dateParsed}</h3><h5>${budget}</h5>`);
     const div2 = $('<div class="grid-x grid-padding-x align-center"></div>');
     //once budget and weather are integrated, we can procedurally attach them
     const weather = '';
-    const budget = '';
     div2.append(morning);
     div2.append(afternoon);
     div2.append(evening);
@@ -54,22 +53,19 @@ function generateDay(date){
 // }
 //where the values are all strings that correspond to the activities that should be done when the weather is good or bad
 function generateCard(dayTime, date){
-    let activityName = '';
-    if(localStorage.getItem(date)!=null){
-        if(goodWeather(dayTime, date)){
-            activityName=localStorage.getItem(date)[dayTime+'Good'];
-        }else{
-            activityName=localStorage.getItem(date)[dayTime+'Bad'];
-        }
+    let activityNameG = 'Add activity';
+    let activityNameB = 'Add activity';
+    const key = `${dayTime}-${date}`;
+    if(localStorage.getItem(key)!=null){
+        console.log(localStorage.getItem(key));
+        const x = JSON.parse(localStorage.getItem(key));
+        activityNameG = x.goodWeather;
+        activityNameB = x.badWeather;
     }
-    if(activityName==''||activityName==null) activityName='Add activity';
-    const card = $(`<div class="cell large-3"><div class="card" style="width: 300px"><div class="card section"><button class="button" type="button" style="height: 200px" id="${dayTime}-${date}">${activityName}</button></div></div></div>`);
+    console.log(key)
+    const card = $(`<div class="cell large-3"><div class="card" style="width: 300px"><h4 class="">${dayTime}</h4><div class="card section"><button class="button" type="button" style="height: 200px" id="${key}">☀️${activityNameG}<br><br><br>⛈️${activityNameB}</button></div></div></div>`);
     return card;
 
-}
-//a shell function for when we get the weather running
-function goodWeather(){
-    //returns true if the weather at the given date and time was projected to be 'good'
 }
 
 
